7e230fafeb552d6ef6d07cdb2361b5d14d17b390
chaomeng <chaomeng04@gmail.com>
2018-04-10 01:14:37 -0700

    setup port and script for heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.set('view engine', 'hbs');
@@ -19,10 +20,6 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.use((req, res, next) => {
-	res.send('temp down');
-});
-
 app.get('/', (req, res) => {
 	res.render('home.hbs', {
 		pageTitle: 'Home Page',
@@ -31,4 +28,6 @@ app.get('/', (req, res) => {
 	});
 });
 
-app.listen(3000);
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
+});
